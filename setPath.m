2f02bf28a006cbe40d72bd2b44f04154b91df219
Christy Yuan Li <liyuanchristy@gmail.com>
2016-04-19 14:24:15 -0400

    update

--- setPath.m
+++
@@ -1,9 +0,0 @@
-% local computer
-% datasetDir = '../dataset/';
-% testsetDir = '../test_data/'; 
-% CLMDir = './';
-
-% server
-datasetDir = '../dataset/'; 
-testsetDir = '../test_data/'; 
-CLMDir = './';
